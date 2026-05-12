d547180ec040122d4d1287880984c364193e0c90
HOPE2301 <yanina_belen_c@hotmail.com>
2026-05-12 15:45:31 -0300

    cambios en el login

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__, template_folder="templates",
             static_folder="static")
@@ -9,18 +9,35 @@ usuarios = []
 
 @app.route('/')
 def inicio():
-    # Esta ruta busca el archivo inicio.html en la carpeta templates
     return render_template('inicio.html')
-
-
-@app.route('/login')
-def registro():
-    return render_template('login.html')
 
 
 @app.route('/formulario')
 def formulario():
     return render_template('formulario.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email= request.form ['email']
+        contraseña= request.form ['contraseña']
+        confirmar= request.form ['confirmar']
+
+    if contraseña != confirmar:
+       flash("Contraseña incorrecta!")
+       return redirect(url_for('formulario'))
+
+    lista_email= []
+    if email in lista_email:
+       flash("Email ya existente")
+       return redirect(url_for('login'))
+       
+
+    return render_template('login.html')
+
+
+
 
 
 if __name__ == '__main__':
