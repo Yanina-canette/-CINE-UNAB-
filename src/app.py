70cd7cc368ac20d054032f1f4fd595d3a4bd9067
HOPE2301 <yanina_belen_c@hotmail.com>
2026-05-13 00:57:56 -0300

    cambios al app.py, logout y otros

--- src/app.py
+++ src/app.py
@@ -1,19 +1,94 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
+from flask import session
+
+
+
 
 app = Flask(__name__, template_folder="templates",
             static_folder="static")
+app.secret_key = "clave_secreta"
 
 # Simulación de una base de datos de socios del cine
-usuarios = []
+usuario = []
+
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="cine_database"
+    
+)
 
 
 @app.route('/')
 def inicio():
-    return render_template('inicio.html')
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    
+    return render_template('inicio.html',
+    usuario= session['usuario'])
+    
+        
+
+    
+@app.route('/logout')
+def logout():
+    session.pop('usuario',None)
+    return redirect(url_for('login'))
 
 
-@app.route('/formulario')
+
+
+@app.route('/formulario', methods=['GET', 'POST'])
 def formulario():
+
+    if request.method == "POST":
+
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        telefono = request.form['telefono']
+        es_estudiante = request.form ['es_estudiante']
+        email = request.form['email']
+        contraseña = request.form['contraseña']
+        confirmar = request.form['confirmar']
+
+        if contraseña != confirmar:
+            flash("las contraseñas no coinciden")
+            return redirect(url_for('formulario'))
+
+        cursor = conexion.cursor()
+
+        cursor.execute(
+            "SELECT * FROM usuarios WHERE email=%s",
+            (email,))
+
+        usuario = cursor.fetchone()
+        cursor.close()
+        if usuario:
+            flash("Email ya existente")
+           
+            return redirect(url_for('formulario'))
+        
+        contraseña_hash = generate_password_hash(contraseña)
+
+        sql = """
+        INSERT INTO usuarios(nombre,apellido,telefono,es_estudiante,email,password_hash)
+        VALUES(%s,%s,%s,%s,%s,%s)
+        """
+
+        valores = (nombre, apellido, telefono, es_estudiante, email, contraseña_hash)
+
+        cursor.execute(sql, valores)
+
+        conexion.commit()
+        cursor.close()
+
+        flash("Usuario registrado correctamente")
+
+        return redirect(url_for('login'))
+
     return render_template('formulario.html')
 
 
@@ -22,24 +97,37 @@ def login():
     if request.method == 'POST':
         email= request.form ['email']
         contraseña= request.form ['contraseña']
-        confirmar= request.form ['confirmar']
 
-    if contraseña != confirmar:
-       flash("Contraseña incorrecta!")
-       return redirect(url_for('formulario'))
+        cursor = conexion.cursor()
 
-    lista_email= []
-    if email in lista_email:
-       flash("Email ya existente")
-       return redirect(url_for('login'))
-       
+        cursor.execute(
+            "SELECT nombre, password_hash FROM usuarios WHERE email=%s",
+            (email,))
+
+        usuario = cursor.fetchone()
+        cursor.close()
+
+        if usuario:
+
+            nombre_db = usuario[0]
+            hash_db = usuario[1]
+
+            if check_password_hash(hash_db, contraseña):
+                session ['usuario'] = nombre_db
+                flash(f"Bienvenido {nombre_db}")
+                return redirect(url_for('inicio'))
+            else:
+                flash("Contraseña incorrecta")
+                return redirect(url_for('login'))
+
+        else:
+            flash("Email no registrado")
+            return redirect(url_for('login'))
 
     return render_template('login.html')
-
-
-
 
 
 if __name__ == '__main__':
     # Esto enciende el servidor web
     app.run(debug=True)
+
