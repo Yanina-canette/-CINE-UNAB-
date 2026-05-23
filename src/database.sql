7a6ecd772b657cc764670ed8f43da587cd7be1b0
Leonel <leonel.fernando.rodriguez07@gmail.com>
2026-05-23 13:56:53 -0300

    correccion de sql

--- src/database.sql
+++ src/database.sql
@@ -12,7 +12,7 @@ CREATE TABLE usuarios (
 CREATE TABLE peliculas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
-    descripcionn TEXT,
+    descripcion TEXT,
     genero VARCHAR(50) NOT NULL,
     duracion_minutos INT NOT NULL,
     clasificacion VARCHAR(20),
@@ -20,16 +20,47 @@ CREATE TABLE peliculas (
     imagen_url VARCHAR(255),
     disponible BOOLEAN DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+
+
+CREATE TABLE salas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    capacidad INT NOT NULL
+);
+
+CREATE TABLE asientos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sala_id INT NOT NULL,
+    fila VARCHAR(5) NOT NULL,
+    numero INT NOT NULL,
+
+    FOREIGN KEY (sala_id) REFERENCES salas(id),
+
+    UNIQUE (sala_id, fila, numero)
+);
+
+CREATE TABLE funciones (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pelicula_id INT NOT NULL,
+    sala_id INT NOT NULL,
+    fecha DATE NOT NULL,
+    hora TIME NOT NULL,
+    
+
+    FOREIGN KEY (pelicula_id) REFERENCES peliculas(id),
+    FOREIGN KEY (sala_id) REFERENCES salas(id)
 );
 
 CREATE TABLE compras (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
-    pelicula_id INT NOT NULL,
+    funcion_id INT NOT NULL,
     cantidad_entradas INT NOT NULL,
     precio_total DECIMAL(10,2) NOT NULL,
-    fecha_compraa TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_compra TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (pelicula_id) REFERENCES peliculas(id)
+    FOREIGN KEY (funcion_id) REFERENCES funciones(id)
 );
